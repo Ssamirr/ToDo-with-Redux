d12c7537974c70348fcebcc90be747ed89bc7dcf
Samir <ssardarlisamir@gmail.com>
2023-01-11 04:19:45 +0400

    change

--- src/pages/ToDoList.js
+++ src/pages/ToDoList.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteItem, update } from '../redux/actions/toDoActionsCreator';
-import { Button, Modal } from 'antd';
+import { Modal } from 'antd';
 
 function ToDoList() {
 
